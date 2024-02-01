4ae2a19a478902e631de0f2e012ef7fe3530a2b7
Donn Jayson Quinto <jaysonquinto1@outlook.com>
2024-02-01 23:11:13 +0800

    fix: remove unnecessary expanded widget

--- lib/components/cart/draggable_sheet.dart
+++ lib/components/cart/draggable_sheet.dart
@@ -11,31 +11,29 @@ class ScrollCart extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double heightWithoutappBarNavBar = MediaQuery.of(context).size.height;
-    return Expanded(
-      child: DraggableScrollableSheet(
-        snap: true,
-        expand: true,
-        builder: (_, scrollController) {
-          return Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(24.0),
-                topRight: Radius.circular(24.0),
-              ),
-              color: Color(0xff2e2e2e),
+    return DraggableScrollableSheet(
+      snap: true,
+      expand: true,
+      builder: (_, scrollController) {
+        return Container(
+          decoration: const BoxDecoration(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(24.0),
+              topRight: Radius.circular(24.0),
             ),
-            child: ListView(
-              controller: scrollController,
-              children: [
-                const ProductQuantity(),
-                Cart(heightWithoutappBarNavBar: heightWithoutappBarNavBar),
-              ],
-            ),
-          );
-        },
-        initialChildSize: 0.38,
-        minChildSize: 0.38,
-      ),
+            color: Color(0xff2e2e2e),
+          ),
+          child: ListView(
+            controller: scrollController,
+            children: [
+              const ProductQuantity(),
+              Cart(heightWithoutappBarNavBar: heightWithoutappBarNavBar),
+            ],
+          ),
+        );
+      },
+      initialChildSize: 0.38,
+      minChildSize: 0.38,
     );
   }
 }
